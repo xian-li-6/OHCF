1c8e8a3cb77b1ed9165bf893fff4e6e82a20c63f
xian-li-6 <3486888718@qq.com>
2025-12-31 10:30:49 +0800

    Update dataloader

--- dataloader.py
+++ dataloader.py
@@ -36,12 +36,36 @@ class Caltech(Dataset):
                 self.view4[idx]), torch.from_numpy(self.view3[idx])], torch.from_numpy(self.labels[idx]), torch.from_numpy(np.array(idx)).long()
 
 
+class ThreeRing(Dataset):
+    def __init__(self, path):
+        # 加载 Three Ring 数据集的 2 个视图数据
+        data1 = scipy.io.loadmat(path + 'ThreeRing.mat')['X1'].astype(np.float32)
+        data2 = scipy.io.loadmat(path + 'ThreeRing.mat')['X2'].astype(np.float32)
+
+        # 加载标签数据
+        labels = scipy.io.loadmat(path + 'ThreeRing.mat')['Y'].transpose()
+
+        # 将数据存储为类的属性
+        self.x1 = data1
+        self.x2 = data2
+        self.y = labels
+
+    def __len__(self):
+        # 返回数据集的样本数
+        return self.x1.shape[0]
+
+    def __getitem__(self, idx):
+        # 返回指定索引的样本（包括 2 个视图的数据和对应的标签）
+        return [torch.from_numpy(self.x1[idx]), torch.from_numpy(self.x2[idx])], \
+            torch.from_numpy(self.y[idx]), torch.from_numpy(np.array(idx)).long()
 def load_data(dataset):
+    if dataset == "Caltech-5V":
         dataset = Caltech('data/Caltech-5V.mat', view=5)
         dims = [40, 254, 928, 512, 1984]
         view = 5
         data_size = 1400
         class_num = 7
+
 
     else:
         raise NotImplementedError
